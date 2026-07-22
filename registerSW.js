1cdba2f0c050a03c1687408121240a5d0200267f
mcarls7 <222330128+mcarls7@users.noreply.github.com>
2026-07-22 05:01:12 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Mesin/sw.js', { scope: '/Mesin/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
