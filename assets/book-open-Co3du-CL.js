14d98ae30db411b3c1a6185177e7345d8b363377
mcarls7 <mcarls7@users.noreply.github.com>
2026-07-25 17:57:57 +0000

    Updates

---
+++ assets/book-open-Co3du-CL.js
@@ -0,0 +1,2 @@
+import{d as e}from"./index-BR_T9INd.js";var t=e(`BookOpen`,[[`path`,{d:`M12 7v14`,key:`1akyts`}],[`path`,{d:`M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z`,key:`ruj8y`}]]);export{t};
+//# sourceMappingURL=book-open-Co3du-CL.js.map
---
+++ assets/message-square-B9IEIRsJ.js
@@ -0,0 +1,2 @@
+import{d as e}from"./index-BR_T9INd.js";var t=e(`MessageSquare`,[[`path`,{d:`M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z`,key:`1lielz`}]]);export{t};
+//# sourceMappingURL=message-square-B9IEIRsJ.js.map
